5e6dec42cee15b6cf00c88e69bf5f0af8b22bfda
Baksultan <amangeldibaksultan@gmail.com>
2023-10-20 19:58:00 +0600

    first commit

---
+++ src/main/java/kz/greetgo/mwapexclcmbfoltqevmn/MwapexclcmbfoltqevmnApplication.java
@@ -0,0 +1,13 @@
+package kz.greetgo.mwapexclcmbfoltqevmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwapexclcmbfoltqevmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwapexclcmbfoltqevmnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/greetgo/mwapexclcmbfoltqevmn/MwapexclcmbfoltqevmnApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.greetgo.mwapexclcmbfoltqevmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwapexclcmbfoltqevmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
